88241ffaae3e40b76c6b286f79cec1f1ca19e35a
SharpSight44 <leoest011@gmail.com>
2022-10-07 19:57:47 -0400

    first one

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Hello How we Do</Text>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'teal',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color:'white',
+    fontSize:40,
+  },
+});
