77651ed988af107022271822f4087ce71834c267
SharpSight44 <leoest011@gmail.com>
2022-10-08 19:17:41 -0400

    miami app

--- App.js
+++ App.js
@@ -1,24 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+
+import WelcomeScreen from './screens/WelcomeScreen';
 
 export default function App() {
+  
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Hello How we Do Yeah!!</Text>
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    
+    <WelcomeScreen/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'teal',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color:'white',
-    fontSize:40,
-  },
-});
---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,96 @@
+import { StyleSheet, Text, View , ImageBackground, Image, TouchableOpacity} from 'react-native';
+import React from 'react';
+import {AntDesign} from '@expo/vector-icons';
+
+export default function WelcomeScreen() {
+  return (
+    <>
+    <ImageBackground style={styles.background} source={{uri:"https://wallpaperaccess.com/full/1099071.jpg"}}>
+     <View style={styles.logoHouse}>
+      <Image style={styles.logo}source={{uri:"https://seeklogo.com/images/M/miami-beach-logo-C677AF8447-seeklogo.com.png"}}/>
+    
+      </View> 
+      <TouchableOpacity>
+        <View style={styles.login}> 
+      <Text style={styles.loginText}> Login   <AntDesign name="login" size={26} color="white" /></Text>
+      
+      </View>
+      </TouchableOpacity>
+      
+        <TouchableOpacity>
+      <View style={styles.register}> 
+      <Text style={styles.registerText}> Register   <AntDesign name="adduser" size={26} color="white" /></Text>
+      </View></TouchableOpacity>
+      
+    </ImageBackground>
+
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  background:{
+    flex:1,
+    justifyContent:"flex-end",
+  
+
+  },
+  login:{
+    width: 200,
+    height: 70,
+    backgroundColor: '#fc5c65',
+    alignSelf:"center",
+    borderRadius: 15,
+    marginBottom:10,
+    borderColor:'white',
+    borderWidth: 3
+
+
+  },
+  loginText:{
+    fontSize: 25,
+    color:'white',
+    alignSelf:"center",
+    marginTop:10,
+    fontWeight:"bold",
+  
+  
+
+  },
+
+  register:{
+    width: 200,
+    height: 70,
+    backgroundColor: '#4ecdc4',
+    alignSelf:"center",
+    borderRadius: 15,
+    marginBottom:10,
+    borderColor:'white',
+    borderWidth: 3,
+    
+
+  },
+  registerText:{
+    fontSize: 25,
+    color:'white',
+    alignSelf:"center",
+    marginTop:10,
+    fontWeight:"bold"
+
+  },
+  
+  logoHouse:{
+    position:"absolute",
+    top: 70,
+    left:150
+   
+
+  },
+  logo:{
+    width:80,
+    height:80
+    
+
+  },
+
+})
