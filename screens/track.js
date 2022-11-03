2f48e183626dfc0e5eb4d138f061927313dc29a9
SharpSight44 <leoest011@gmail.com>
2022-11-02 21:27:36 -0400

    layout phase 1

--- screens/track.js
+++ screens/track.js
@@ -9,20 +9,58 @@ const MainPage = () => {
   return (
     <>
      <View style={{ width:'100%',height:'10%', backgroundColor:'#c3fad3'}}> 
-<View style={{width:20,height:20, backgroundColor:'09e34a'}}>
-<Text style={{}}>
-  +
+<View style={{width:45, height:45, backgroundColor:'#09e34a', justifyContent:'center', alignItems:'center'}}>
+<Text style={{fontSize:50}}>+
 </Text>
 </View>
 </View>
     <View style={{backgroundColor:'#50a2a3', height:'75%',  alignItems:'center'}}>
     <View style={styles.menu}> 
+    <Text style={{textAlign:'right'}}>
+    Last Visited 11/2/2022
+      </Text>
+    <Text>
+      Wednesday {'\n'}
+      1. I have to create my million dollar App {'\n'}
+      2. I Get to PLay VR {'\n'}
+      3. Make sure to go to sleep on time 
+    </Text>
     </View>
     <View style={styles.menu}> 
+    <Text style={{textAlign:'right'}}>
+    Last Visited 11/2/2022
+      </Text>
+    <Text>
+  
+      Wednesday {'\n'}
+      1. I have to create my million dollar App {'\n'}
+      2. I Get to PLay VR {'\n'}
+      3. Make sure to go to sleep on time 
+    </Text>
     </View>
     <View style={styles.menu}> 
+      <Text style={{textAlign:'right'}}>
+    Last Visited 11/2/2022
+      </Text>
+    <Text  >
+      Wednesday {'\n'}
+      1. I have to create my million dollar App {'\n'}
+      2. I Get to PLay VR {'\n'}
+      3. Make sure to go to sleep on time 
+      
+   
+    </Text>
     </View>
     <View style={styles.menu}> 
+    <Text style={{textAlign:'right'}}>
+    Last Visited 11/2/2022
+      </Text>
+    <Text>
+      Wednesday {'\n'}
+      1. I have to create my million dollar App {'\n'}
+      2. I Get to PLay VR {'\n'}
+      3. Make sure to go to sleep on time 
+    </Text>
     </View>
     </View>
     <View style={{ width:'100%',height:'15%', backgroundColor:'#f2dc9d'}}> 
@@ -39,8 +77,10 @@ const styles = StyleSheet.create({
      margin: 10,
      backgroundColor: '#ffffff',
      height:100,
-     width: '90%'
+     width: '90%',
+     
 
     
-  }
+  }, 
+ 
 })
